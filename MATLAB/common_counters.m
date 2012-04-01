82f450a5e83451db48a9e75fb39e772ae7b10c07
Cha Li <chavli@gmail.com>
2012-04-01 04:40:11 -0400

    added svm and decision tree models to judge fitness of extracted counters

--- MATLAB/common_counters.m
+++ MATLAB/common_counters.m
@@ -15,22 +15,25 @@ for f=1:size(infiles, 1)
 end
 
 [dist, centers] = hist(all_counters, max(all_counters));
-%hist(all_counters, max(all_counters));
+hist(all_counters, max(all_counters));
 
 dist = vertcat(1:139, dist)';
 dist = sortrows(dist, -2);
+%dist
 
-features = 3;
+features = 20;
 top = dist(1:features, :);
+names = {};
 
 disp('common counters');
 for i=1:features
     disp(sprintf('%d \t %s', top(i, 1), counter_names{1}{top(i, 1)}));
+    names = horzcat(names, counter_names{1}{top(i, 1)});
 end
 
 
 %%
-%training 
+%prepare data for training 
 
 thread_data = csvread('./training_data/all-training.csv');
 phase_data = csvread('./training_data/training-phase.csv');
@@ -53,17 +56,11 @@ norm_phase = normalize_m(phase_data, 0);
     IPC = RETIRED-INSTRUCTIONS / CPU-CLK-UNHALTED
 %}
 
-%ctrs = [];
+%ctrs = [6];
 ctrs = top(:, 1)';
 
 figure
 plot(thread_data(:, ctrs));
-%figure
-%ipc = thread_data(:, 47) ./ thread_data(:, 13);
-%plot([thread_data(:, [6]), ipc]);
-%plot(norm_data(:, 47) ./ norm_data(:, 13));
-
-
 
 %only use the counters specified by the extraction
 cols = [ctrs, size(norm_data, 2)];
@@ -76,19 +73,37 @@ trials = 100;
 avg_err = 0;
 avg_conf = [0,0;0,0];
 
+
+
+%%
+% training and testing different models
+
 for t=1:trials
     [train_data, test_data] = divideset2(norm_data, .25);
     attrs_n = size(test_data, 2);
 
     %logistic regression 
-    weights_v = online_glr(train_data, size(train_data, 1), 0, []);
-    [predict_y, posterior_y] = binary_logistic_predict(test_data, weights_v);
+    %weights_v = online_glr(train_data, size(train_data, 1), 0, []);
+    %[predict_y, posterior_y] = binary_logistic_predict(test_data, weights_v);
 
+    %support vector machine (SVM)
+    %[weights_v, bias] = svml(train_data(:, 1:attrs_n - 1), train_data(:, attrs_n), 5);
+    %[predict_y, posterior_y] = binary_svm_predict(test_data(:,1:attrs_n-1), weights_v, bias); 
+    
+    %decision tree
+    [train_data, test_data] = divideset2(norm_data, .25);
+    attrs_n = size(test_data, 2);
+    tree = classregtree(train_data(:, 1:attrs_n - 1), train_data(:, attrs_n), 'names', names, 'method', 'classification');
+    predict_y = cell2mat(eval(tree, test_data(:, 1:attrs_n - 1)));
+    predict_y = str2num(predict_y);
+
+        
     [conf_m, stats_m] = binary_confusion_matrix(test_data(:,attrs_n), predict_y);
     [err_count, err_rate] = misclass_count(test_data(:,attrs_n), predict_y);
     
     avg_err = avg_err + err_rate;
     avg_conf = avg_conf + conf_m; 
 end
+
 avg_conf ./ trials
 avg_err / trials
--- MATLAB/label_training.m
+++ MATLAB/label_training.m
@@ -1,12 +1,12 @@
 
 
-infiles = dir(fullfile('./training_data','*.csv'));
+infiles = dir(fullfile('./training_prep','*.csv'));
 
 for f=1:size(infiles, 1)
-    filename = sprintf('./training_data/%s', infiles(f).name);
+    filename = sprintf('./training_prep/%s', infiles(f).name);
     data = csvread(filename);
     norm_data = normalize_m(data, 1);
-    labels = norm_data(:, 35) > 0;
+    labels = norm_data(:, 35) > 0; %use attribute 35 as basis for judgment
     data = horzcat(data, labels);
     dlmwrite(sprintf('./training_data/training-%s', infiles(f).name), data);
 end
