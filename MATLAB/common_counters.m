2f2f9bbea3469312f57d1b842245cb791c53ee24
Cha Li <chavli@gmail.com>
2012-04-11 18:18:41 -0400

    minor changes to common_counters

--- MATLAB/common_counters.m
+++ MATLAB/common_counters.m
@@ -43,12 +43,13 @@ dist = sortrows(dist, -2);
 %}
 
 %disp(dist(1:10, :));
-features = 10;
+features = 4;
 %foo = [7, 9, 3, 4];
 %foo = [3, 9];
 %top = dist(foo, :);
-top = dist(1:features, :);
-%top = [6,0;47,0;13,0;68,0;];
+%top = dist(1:features, :);
+top = [6,0;47,0;13,0;68,0;];
+%top = [6,0;13,0;];
 names = {};
 
 disp('common counters');
@@ -80,13 +81,12 @@ norm_data = norm_data(:, cols);
 %%
 % training and testing different models
 disp('----- extracted results -----');
-all_errs = zeros(1,100);
+all_errs = zeros(3,100);
 
-for trials=1:1
+for trials=1:100
     K = 10;
     groupings = crossvalind('Kfold', (1:size(norm_data, 1)), K);
-    avg_conf = [0,0; 0,0];
-    avg_err = 0;
+    avg_err = zeros(3, 1);
     
     for k=1:K
 
@@ -94,37 +94,40 @@ for trials=1:1
         train_data = norm_data(find(groupings ~= k), :);
         attrs_n = size(test_data, 2);
 
-        %logistic regression 
+        %logistic regression 1
         weights_v = online_glr(train_data, size(train_data, 1), 0, []);
         [predict_y, posterior_y] = binary_logistic_predict(test_data, weights_v);
+        [err_count, err_rate] = misclass_count(test_data(:,attrs_n), predict_y);        
+        avg_err(1, 1) = avg_err(1, 1) + err_rate;
         
-        %support vector machine (SVM)
-        %[weights_v, bias] = svml(train_data(:, 1:attrs_n - 1), train_data(:, attrs_n), 5);
-        %[predict_y, posterior_y] = binary_svm_predict(test_data(:,1:attrs_n-1), weights_v, bias); 
-
-        %decision tree
-        %tree = classregtree(train_data(:, 1:attrs_n - 1), train_data(:, attrs_n), 'names', names, 'method', 'classification');    
-        %predict_y = cell2mat(eval(tree, test_data(:, 1:attrs_n - 1)));
-        %predict_y = str2num(predict_y);
-
-
-        [conf_m, stats_m] = binary_confusion_matrix(test_data(:,attrs_n), predict_y);
+        %support vector machine (SVM) 2
+        [weights_v, bias] = svml(train_data(:, 1:attrs_n - 1), train_data(:, attrs_n), 5);
+        [predict_y, posterior_y] = binary_svm_predict(test_data(:,1:attrs_n-1), weights_v, bias); 
         [err_count, err_rate] = misclass_count(test_data(:,attrs_n), predict_y);
+        avg_err(2, 1) = avg_err(2, 1) + err_rate;
+        
+        %decision tree 3
+        tree = classregtree(train_data(:, 1:attrs_n - 1), train_data(:, attrs_n), 'names', names, 'method', 'classification');    
+        predict_y = cell2mat(eval(tree, test_data(:, 1:attrs_n - 1)));
+        predict_y = str2num(predict_y);
+        [err_count, err_rate] = misclass_count(test_data(:,attrs_n), predict_y);
+        avg_err(3, 1) = avg_err(3, 1) + err_rate;
 
-        avg_err = avg_err + err_rate;
-        avg_conf = avg_conf + conf_m; 
     end
     %disp(avg_conf / K);
     %disp(avg_err / K);
-    all_errs(trials) = avg_err / K;
+    all_errs(:, trials) = avg_err / K;
 end
 
-disp(mean(all_errs));
-disp(std(all_errs));
+disp('average error');
+disp(mean(all_errs, 2));
+disp('average std');
+disp(std(all_errs, 0, 2));
 
 
 %%
 %independent run of the decision tree against all counters of phase data
+%{
 disp('----- baseline -----');
 all_errs = zeros(1,100);
 
@@ -156,5 +159,8 @@ all_errs = zeros(1,100);
     %disp(avg_err / K);
     all_errs(trials) = avg_err / K;
 %end
+disp('average error');
 disp(mean(all_errs));
+disp('average std');
 disp(std(all_errs));
+%}
